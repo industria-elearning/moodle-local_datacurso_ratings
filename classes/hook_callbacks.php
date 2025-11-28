332b0f6929060a025ba69de9076ba44f6eaa7056
JosueCondori-17 <josue@datacurso.com>
2025-11-28 16:10:51 -0500

    fix: fix linter file hook_callbacks

--- classes/hook_callbacks.php
+++ classes/hook_callbacks.php
@@ -22,7 +22,6 @@ use core\hook\output\before_footer_html_generation;
  * Hook callbacks for local_datacurso_ratings.
  *
  * @package    local_datacurso_ratings
- * @category   Tool
  * @copyright  2025 Industria Elearning
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
